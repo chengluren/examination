34b610433676e1a5b266c98178e9471c8bd383e8
lcheng <chengluren@126.com>
2014-04-11 17:33:24 +0800

    增加ExamController

--- examination-admin/src/main/java/org/dreamer/examination/web/controller/ExamController.java
+++ examination-admin/src/main/java/org/dreamer/examination/web/controller/ExamController.java
@@ -2,8 +2,10 @@ package org.dreamer.examination.web.controller;
 
 import org.codehaus.jackson.map.util.JSONPObject;
 import org.dreamer.examination.business.ExaminationManager;
+import org.dreamer.examination.entity.CommitAnswerVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,12 +24,47 @@ public class ExamController {
     private ExaminationManager examManager;
 
     /**
-     * 学生参加考试
+     * <p>学生参加考试。根据学生专业到后台匹配考试模板，
+     * 匹配到模板后,为学生返回试题列表，考试Id、试卷号</p>
      */
-    @RequestMapping(value = "/",method = RequestMethod.POST)
+    @RequestMapping(value = "/newExam")
     @ResponseBody
-    public JSONPObject takeExamination(String staffId,String major,long tempId){
-        //examManager.newExamination(tempId,staffId,)
+    public JSONPObject takeExamination(String staffId,String major){
+
         return null;
     }
+
+    /**
+     * <p>学生提交答案，返回最终得分。此为一次性提交所有答案</p>
+     * <p>提交的答案内容格式为：
+     * {
+     *  examId:1
+     *  paperId:2
+     *  answers:{
+     *      1111: A
+     *      2222:B
+     *  }
+     * }</p>
+     * @param answer
+     * @return
+     */
+    @RequestMapping(value = "/commitAnswer")
+    @ResponseBody
+    public JSONPObject commitAnswer(CommitAnswerVO answer){
+
+       return null;
+    }
+
+    /**
+     * 按题型提交答案
+     * @param answer
+     * @param type
+     * @return
+     */
+    @RequestMapping(value = "/commitAnswer/{type}")
+    public JSONPObject commitAnswer(@PathVariable("type")String type,CommitAnswerVO answer){
+      return null;
+    }
+
+    //public JSONPObject
 }
---
+++ examination-business/src/main/java/org/dreamer/examination/entity/CommitAnswerVO.java
@@ -0,0 +1,40 @@
+package org.dreamer.examination.entity;
+
+import java.util.Map;
+
+/**
+ * @author lcheng
+ * @version 1.0
+ *          ${tags}
+ */
+public class CommitAnswerVO {
+
+    private Long examId;
+    private Long paperId;
+
+    private Map<Long,String> answers;
+
+    public Long getExamId() {
+        return examId;
+    }
+
+    public void setExamId(Long examId) {
+        this.examId = examId;
+    }
+
+    public Long getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(Long paperId) {
+        this.paperId = paperId;
+    }
+
+    public Map<Long, String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Map<Long, String> answers) {
+        this.answers = answers;
+    }
+}
